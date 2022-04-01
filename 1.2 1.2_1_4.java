b9691113bbcc4fcf3c960652bb247b4034039b98
s22620 <73877454+s22620@users.noreply.github.com>
2022-04-01 17:57:28 +0200

    Add files via upload

---
+++ 1.2 1.2_1_4.java
@@ -0,0 +1,12 @@
+import java.text.DecimalFormat;
+import static java.lang.Math.*;
+
+public class Main {
+    private static final DecimalFormat DF = new DecimalFormat("#.##");
+
+    public static void main(String[] args) {
+        double wynik = sqrt(PI);
+
+        System.out.print(DF.format(wynik));
+    }
+}
