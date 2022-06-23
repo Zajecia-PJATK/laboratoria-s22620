856680b832471b614a4fbc46f8e8ebd4802c306a
s22620 <73877454+s22620@users.noreply.github.com>
2022-06-23 20:48:45 +0200

    Add files via upload

---
+++ Laboratorium_1/1.3 1.3_1.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String sentence = in.nextLine().toLowerCase();
+
+        boolean flag = true;
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (sentence.indexOf(i) == -1) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
